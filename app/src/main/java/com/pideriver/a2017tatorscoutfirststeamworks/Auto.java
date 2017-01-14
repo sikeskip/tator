18ced8d27b64bf5a277d0cf65245298e85efbd31
Kevin Sikes <kevinsikes@cableone.net>
2017-01-14 14:09:25 -0700

    added activities for auto,teleop,comments,mach setup, and refactored main activity to be scout sign in

--- app/src/main/java/com/pideriver/a2017tatorscoutfirststeamworks/Auto.java
+++ app/src/main/java/com/pideriver/a2017tatorscoutfirststeamworks/Auto.java
@@ -3,11 +3,11 @@ package com.pideriver.a2017tatorscoutfirststeamworks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Auto extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_auto);
     }
 }
